8b579353ccbafa2d4a5e37cb428bc862ef8654a0
devadharshi <135523306+devadharshi@users.noreply.github.com>
2023-06-21 18:36:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import WordCounter from './components/wordcounter';
+
+function App() {
+  return (
+    <>
+    <WordCounter/>
+    </>
+
+  );
+}
+
+export default App;
